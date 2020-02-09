e9c0127d50845308e461a8e3f9c36818e1f5575d
Nigel Hing <nigelhing1@gmail.com>
2020-02-09 23:49:56 +0800

    first commit

---
+++ BackEnd/Models/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace Backend.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Speaker> Speakers { get; set; }
+    }
+}
+
+
+
